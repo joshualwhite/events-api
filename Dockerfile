43f715a579b0ea24ad9ef6afd3799528b16de49e
Josh <jwhite@bushelpowered.com>
2022-09-09 17:44:00 -0500

    configured dynamodb + events table

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM node:12-alpine
 
 WORKDIR /usr/src/app
 
+ENV AWS_ACCESS_KEY_ID=id
+ENV AWS_SECRET_ACCESS_KEY=key
+ENV AWS_REGION=local
+ENV AWS_ENDPOINT=http://dynamodb:8000
+
 COPY package*.json ./
 
 RUN npm install
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const dynamodb = require('dynamodb');
+const Joi  = require('joi')
 
 const app = express()
 const port = 3000 
 
+dynamodb.AWS.config.update({  
+    region: process.env.AWS_REGION,
+    endpoint: process.env.AWS_ENDPOINT,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID
+});
+
+const Event = dynamodb.define('Event', {
+    hashKey : 'id',
+  
+    schema : {
+      id            : dynamodb.types.uuid(),
+      description   : Joi.string(),
+      event_time    : Joi.date(),
+      customer_id   : Joi.string(),
+      type          : Joi.string(),
+      details       : Joi.string(),
+    }
+});
+  
 app.use(function (req, res, next) {
     res.header("content-type",'application/json');
     next();
@@ -17,5 +39,13 @@ app.post("/events", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Events app listening on port ${port}`)
+    dynamodb.createTables(function(err) {
+        if (err) {
+          console.log('Terminal Application Error. Could not create DynamoDb tables: ', err);
+          process.exit() 
+        } else {
+          console.log('Tables have been created');
+          console.log(`Events app listening on port ${port}`)
+        }
+    });
 })
